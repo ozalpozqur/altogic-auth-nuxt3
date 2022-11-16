043f0ac456f1ed74b4f940f341b3b73dab77c991
Özgür ÖZALP <ozgur@altogic.com>
2022-11-16 11:40:36 +0300

    Update logout.js

--- server/api/auth/logout.js
+++ server/api/auth/logout.js
@@ -3,6 +3,6 @@ import altogic from '~/libs/altogic';
 export default defineEventHandler(async event => {
 	const token = getCookie(event, 'session_token');
 	await altogic.auth.signOut(token);
-	deleteCookie(event, 'session_token');
+	altogic.auth.removeSessionCookie(event.req, event.res);
 	await sendRedirect(event, '/login');
 });
